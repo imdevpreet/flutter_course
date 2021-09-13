b33d738d1b0ad373498e41d2fe64354c4786704f
dev stack <gauravsingh@dcubetechnologies.com>
2021-09-13 20:50:26 +0530

    erroring

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter_catlog/pages/home_page.dart';
 import 'package:flutter_catlog/pages/login_page.dart';
 import 'package:flutter_catlog/utilities/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-//testing code
+//erroor
 void main() {
   runApp(MyApp());
 }
